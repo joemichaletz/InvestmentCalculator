27c67d184ef175a3cc3c2b63cb6dd4444f52d803
joemichaletz <55150058+joemichaletz@users.noreply.github.com>
2019-10-15 08:11:54 -0500

    ch ch ch ch changes

--- src/Investment.java
+++ src/Investment.java
@@ -19,12 +19,32 @@ public class Investment
 		//return the current value of this investment taking
 		//time and an average 8% yearly or 0.67% monthly return
 		//over afterHowManyMonths number of months
+		//this.theCon
+		double total = 0;
+		for(int i = 0; i < afterHowManyMonth; i++)
+		{
+			total *= 1.0067;
+			for(int l = 0; l < theContributions.size(); l++)
+			{
+				if(theContributions.get(l).monthNumber == i)
+				{
+					total += theContributions.get(l).amount;
+				}
+			}
+		}
+		return total;
 	}
 	
 	double getTotalContributions()
 	{
 		//return the total amount ever investment not taking into
 		//account interest.
+		double dollars = 0;
+		for(int i = 0; i < theContributions.size(); i++)
+		{
+			dollars += theContributions.get(i).amount;
+		}
+		return dollars;
 	}
 	
 }
